81fa407509ad0fe7b5581a69ee82d6d5b901e548
akdemirsafak <akdemirsafak@gmail.com>
2024-05-01 22:30:38 +0300

    StrategyPattern 1. kullanım senaryosu

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,61 @@
+﻿//Strategy Design Pattern -- Behavioral Design Pattern
+
+//Çok sık kullanılan desgin patternlerinden biridir.
+//Yaratılmış bir nesne üzerinden kullanmış olduğu aksiyonları değiştirebildiğimiz pattern'dir.
+
+
+var ch= new Character(new AggressiveStrategy());
+ch.ApplyAttackStrategy();
+
+Console.WriteLine("---------");
+
+ch.SetCombatStrategy(new DefensiveStrategy());
+ch.ApplyAttackStrategy();
+
+Console.ReadLine();
+
+class Character
+{
+    private ICombatStrategy _combatStrategy;
+
+    public Character(ICombatStrategy combatStrategy)
+    {
+        _combatStrategy = combatStrategy;
+    }
+    public Character()
+    {
+        
+    }
+
+    public void SetCombatStrategy(ICombatStrategy combatStrategy)
+    {
+        _combatStrategy = combatStrategy;
+    }
+
+    public void ApplyAttackStrategy()
+    {
+        _combatStrategy.Attack();
+    }
+}
+
+
+interface ICombatStrategy
+{
+    void Attack();
+}
+
+class AggressiveStrategy : ICombatStrategy
+{
+    public void Attack()
+    {
+        Console.WriteLine("Aggressive Attack");
+    }
+}
+
+class DefensiveStrategy : ICombatStrategy
+{
+    public void Attack()
+    {
+        Console.WriteLine("Defensive Attack");
+    }
+}
