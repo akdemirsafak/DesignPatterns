c2130f39eba4c3e559a170b9065d6f185c4d2b46
akdemirsafak <akdemirsafak@gmail.com>
2024-05-01 23:16:19 +0300

    Real life StrategyPattern Example

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -4,58 +4,113 @@
 //Yaratılmış bir nesne üzerinden kullanmış olduğu aksiyonları değiştirebildiğimiz pattern'dir.
 
 
-var ch= new Character(new AggressiveStrategy());
-ch.ApplyAttackStrategy();
 
-Console.WriteLine("---------");
+//Gerçek hayat örneği  : Bir ödeme sistemi kullanıldığını düşünelim. Ödeme işlemleri sırasında ödeme tipi seçilebilir.
+//Ödeme tipi seçildiğinde ödeme tipine göre işlem yapılır.
+//Örneğin kredi kartı ile ödeme yapılacaksa kredi kartı bilgileri girilir ve ödeme yapılır.
+//Eğer havale ile ödeme yapılacaksa havale bilgileri girilir ve ödeme yapılır. Bu durumda Strategy Pattern kullanılabilir.
+//Ödeme tipine göre işlem yapılacaksa Strategy Pattern kullanılabilir.
 
-ch.SetCombatStrategy(new DefensiveStrategy());
-ch.ApplyAttackStrategy();
 
-Console.ReadLine();
-
-class Character
+var paymentOptions = new PaymentOptions
 {
-    private ICombatStrategy _combatStrategy;
+    CardNumber = "123456789",
+    CardHolderName = "Ali Veli",
+    CardExpirationDate = "12/22",
+    CardCvv = "123",
+    Amount = 100
+};
 
-    public Character(ICombatStrategy combatStrategy)
+var paymentService = new PaymentService();
+
+
+do
+{
+    Console.Write("Ödeme yapılacak bankayı seçiniz : (1: Ziraat, 2: Garanti, 3: İş Bankası");
+    var bank = Console.ReadLine();
+
+    IPaymentService bankPaymentService = null;
+
+    switch (bank)
     {
-        _combatStrategy = combatStrategy;
+        case "1":
+            bankPaymentService = new ZiraatBankPaymentService();
+            break;
+        case "2":
+            bankPaymentService = new GarantiBankPaymentService();
+            break;
+        case "3":
+            bankPaymentService = new IsBankPaymentService();
+            break;
+        default:
+            Console.WriteLine("Geçersiz banka seçimi");
+            break;
     }
-    public Character()
+    paymentService.SetPaymentService(bankPaymentService);
+    paymentService.PayViaStrategy(paymentOptions);
+    
+} while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+
+class PaymentService
+{
+    private IPaymentService _paymentService;
+
+    public PaymentService()
     {
         
     }
-
-    public void SetCombatStrategy(ICombatStrategy combatStrategy)
+    public PaymentService(IPaymentService paymentService)
     {
-        _combatStrategy = combatStrategy;
+        _paymentService = paymentService;
+    }
+    public void SetPaymentService(IPaymentService paymentService) //Anlık strategy değişikliği yapabilmek için sadece ctor olmamalı
+    {
+        _paymentService = paymentService;
     }
 
-    public void ApplyAttackStrategy()
+    public bool PayViaStrategy(PaymentOptions paymentOptions)
     {
-        _combatStrategy.Attack();
-    }
-}
-
-
-interface ICombatStrategy
-{
-    void Attack();
-}
-
-class AggressiveStrategy : ICombatStrategy
-{
-    public void Attack()
-    {
-        Console.WriteLine("Aggressive Attack");
+        return _paymentService.Pay(paymentOptions);
     }
 }
 
-class DefensiveStrategy : ICombatStrategy
+
+interface IPaymentService
 {
-    public void Attack()
+    bool Pay(PaymentOptions paymentOptions);
+}
+
+class PaymentOptions
+{
+    public string CardNumber { get; set; }
+    public string CardHolderName { get; set; }
+    public string CardExpirationDate { get; set; }
+    public string CardCvv { get; set; }
+    public decimal Amount { get; set; }
+}
+
+class ZiraatBankPaymentService : IPaymentService
+{
+    public bool Pay(PaymentOptions paymentOptions)
     {
-        Console.WriteLine("Defensive Attack");
+        Console.WriteLine("Ziraat Bank Payment Service");
+        return true;
+    }
+}
+class GarantiBankPaymentService : IPaymentService
+{
+    public bool Pay(PaymentOptions paymentOptions)
+    {
+        Console.WriteLine("Garanti Bank Payment Service");
+        return true;
+    }
+}
+class IsBankPaymentService : IPaymentService
+{
+    public bool Pay(PaymentOptions paymentOptions)
+    {
+        Console.WriteLine("Is Bank Payment Service");
+        return true;
     }
 }
